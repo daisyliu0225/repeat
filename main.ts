b332038e8978bf420ba31908024a2176f2ab65da
daisyliu0225 <50540881+daisyliu0225@users.noreply.github.com>
2022-07-20 22:10:43 +0800

    creative stuff

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Square)
     basic.pause(500)
     basic.showIcon(IconNames.SmallSquare)
@@ -15,4 +15,24 @@ basic.forever(function () {
     basic.pause(500)
     basic.showIcon(IconNames.Happy)
     basic.pause(100)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(500)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(500)
+    basic.clearScreen()
+    basic.pause(500)
+    basic.showIcon(IconNames.Surprised)
+    basic.pause(100)
+    basic.clearScreen()
 })
